c7c194cae98cf82dbea35c4982fc0cbebb98749d
mathe <matheus.felipe.bota.10@gmail.com>
2024-06-06 20:13:19 -0400

    essencias

---
+++ src/main/java/com/hotel/lakeside/LakeSideApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.lakeside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakeSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakeSideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel/lakeside/LakeSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.lakeside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakeSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
