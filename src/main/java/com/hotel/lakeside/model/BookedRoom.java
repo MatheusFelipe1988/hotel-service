d6f6076e21fddae9c2713718e5a5ff1ba3dbef10
mathe <matheus.felipe.bota.10@gmail.com>
2024-06-10 15:26:06 -0400

    Metodos e Instancias prontas

--- src/main/java/com/hotel/lakeside/model/BookedRoom.java
+++ src/main/java/com/hotel/lakeside/model/BookedRoom.java
@@ -1,4 +1,67 @@
 package com.hotel.lakeside.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @Column(name = "check_in")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_out_date")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_fullname")
+    private String guestFullName;
+
+    @Column(name = "guest_email")
+    private String guestEmail;
+
+    @Column(name = "adults")
+    private int numberOfAdults;
+
+    @Column(name = "childrens")
+    private int numberOfChildren;
+
+    @Column(name = "total_guest")
+    private int totalFullguest;
+
+    @Column(name = "confirmation_code")
+    private String bookingConfirmationCode;
+
+    @JoinColumn(name = "room_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Room room;
+
+    public void totalCalculateNumberOfGuest(){
+        this.totalFullguest = this.numberOfAdults + numberOfChildren;
+    }
+
+    public void setNumberOfAdults(int numberOfAdults) {
+        this.numberOfAdults = numberOfAdults;
+        totalCalculateNumberOfGuest();
+    }
+
+    public void setNumberOfChildren(int numberOfChildren) {
+        this.numberOfChildren = numberOfChildren;
+        totalCalculateNumberOfGuest();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
