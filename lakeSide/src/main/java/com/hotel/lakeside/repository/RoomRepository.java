060c323a75151a8f2a586af41097b532d0b60482
MatheusFel33 <104394609+MatheusFelipe1988@users.noreply.github.com>
2024-06-11 17:49:55 -0400

    Update
    Room finalizada

---
+++ lakeSide/src/main/java/com/hotel/lakeside/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.hotel.lakeside.repository;
+
+import com.hotel.lakeside.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+}
---
+++ lakeSide/src/main/java/com/hotel/lakeside/response/RoomResponse.java
@@ -0,0 +1,40 @@
+package com.hotel.lakeside.response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class RoomResponse {
+    private Long id;
+
+    private String roomType;
+
+    private BigDecimal roomPrice;
+
+    private boolean isBooked;
+
+    private String foto;
+
+    private List<BookingResponse> bookings;
+
+    public RoomResponse(Long id, String roomType, BigDecimal roomPrice) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+    }
+
+    public RoomResponse(Long id, String roomType, BigDecimal roomPrice,
+                        boolean isBooked, byte[] fotoByte, List<BookingResponse> bookings) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.isBooked = isBooked;
+        this.foto = fotoByte != null ? Base64.encodeBase64String(fotoByte) : null;
+        this.bookings = bookings;
+    }
+}
