75bea25562de1cf955f04670cfb5da980298cef6
mathe <matheus.felipe.bota.10@gmail.com>
2024-06-16 09:22:47 -0400

    novos métodos implementados

--- src/main/java/com/hotel/lakeside/service/RoomServiceImple.java
+++ src/main/java/com/hotel/lakeside/service/RoomServiceImple.java
@@ -1,5 +1,6 @@
 package com.hotel.lakeside.service;
 
+import com.hotel.lakeside.exception.ResourceNotFoundException;
 import com.hotel.lakeside.model.Room;
 import com.hotel.lakeside.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,8 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,12 +27,44 @@ public class RoomServiceImple implements IRoomService{
         Room room = new Room();
         room.setRoomType(roomType);
         room.setRoomPrice(roomPrice);
-        if (!file.isEmpty() ){
+        if (!file.isEmpty()){
             byte[] fotoByte = file.getBytes();
             Blob fotoBlob = new SerialBlob(fotoByte);
             room.setFoto(fotoBlob);
         }
 
         return repository.save(room);
+    }
+
+    @Override
+    public List<String> getAllRoomTypes() {
+        return repository.findDistinctRoomTypes();
+    }
+
+    @Override
+    public List<Room> getAllRoms() {
+        return repository.findAll();
+    }
+
+    @Override
+    public byte[] getRoomFotoByRoomId(Long roomId) throws SQLException {
+        Optional<Room> theRoom = repository.findById(roomId);
+        if(theRoom.isEmpty()){
+            throw new ResourceNotFoundException("Sorry, not found room");
+        }
+        Blob fotoBlob = theRoom.get().getFoto();
+        if (fotoBlob != null){
+            return  fotoBlob.getBytes(1, (int) fotoBlob.length());
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteRoom(Long roomId) {
+        Optional<Room> theRoom = repository.findById(roomId);
+        if(theRoom.isPresent()){
+            repository.deleteById(roomId);
+        }
+
     }
 }
