bef8c41de16e02c64cfed653a8f0559d92047f21
mathe <matheus.felipe.bota.10@gmail.com>
2024-06-16 09:22:16 -0400

    novas exceptiions

---
+++ src/main/java/com/hotel/lakeside/exception/FotoRetrivalException.java
@@ -0,0 +1,7 @@
+package com.hotel.lakeside.exception;
+
+public class FotoRetrivalException extends RuntimeException {
+    public FotoRetrivalException(String message) {
+        super(message);
+    }
+}
