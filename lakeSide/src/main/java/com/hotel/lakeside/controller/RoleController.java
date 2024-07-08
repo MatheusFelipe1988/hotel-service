5fa1059d92b26f35d2b00f720c8ae42b9a007d05
MatheusFel33 <104394609+MatheusFelipe1988@users.noreply.github.com>
2024-07-07 20:15:15 -0400

    Update
    Role e User controller,service e model finalizada

--- lakeSide/src/main/java/com/hotel/lakeside/controller/RoleController.java
+++ lakeSide/src/main/java/com/hotel/lakeside/controller/RoleController.java
@@ -2,6 +2,7 @@ package com.hotel.lakeside.controller;
 
 import com.hotel.lakeside.exception.RoleAlreadyExistException;
 import com.hotel.lakeside.model.Role;
+import com.hotel.lakeside.model.User;
 import com.hotel.lakeside.service.IRoleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,7 +22,7 @@ public class RoleController {
         return new ResponseEntity<>(roleService.getRoles(), HttpStatus.FOUND);
     }
 
-    @PostMapping("")
+    @PostMapping("/create-new-role")
     public ResponseEntity<String> createRole(@RequestBody Role role){
         try {
             roleService.createRole(role);
@@ -29,5 +30,25 @@ public class RoleController {
         }catch(RoleAlreadyExistException e){
             return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
         }
+    }
+
+    @DeleteMapping("/delete/{roleId}")
+    public void deleteRole(@PathVariable("roleId") Long roleId){
+        roleService.deleteRole(roleId);
+    }
+
+    @PostMapping("/remove-all-users-from-role/{roleId}")
+    public Role removeAllUsersFromRole(@PathVariable("roleId") Long roleId){
+        return roleService.removeAllUsersFromRole(roleId);
+    }
+
+    @PostMapping("remove-user-to-role")
+    public User removeUserFromRole(@RequestParam("userId") Long userId, @RequestParam("roleId") Long roleId){
+        return roleService.removeUserFromRole(userId, roleId);
+    }
+
+    @PostMapping("/assign-user-to-role")
+    public User assignUserToRole(@RequestParam("userId") Long userId, @RequestParam("roleId") Long roleId){
+        return roleService.assignRoleToUser(userId, roleId);
     }
 }
